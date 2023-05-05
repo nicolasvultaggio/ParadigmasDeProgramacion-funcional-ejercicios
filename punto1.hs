8401be7e45b60e72b9d342949a39ddb6b2b35de6
nicolasvultaggio <pvultaggio@frba.utn.edu.ar>
2023-05-05 11:36:37 -0300

    subo primera resolucion del punto 1, todavía creo que se le pueden hacer muchas mejoras

--- punto1.hs
+++ punto1.hs
@@ -20,14 +20,33 @@ tipoDeFelicidad unaPersona
     | felicidonios unaPersona <= 50 = "Poco Feliz"
     | otherwise = "Moderadamente Feliz"
 
+
+gradoDeAmbicion :: Persona -> Int
+gradoDeAmbicion unaPersona = enFuncionDeLaFelicidad  unaPersona (felicidonios unaPersona) (edad unaPersona) 2  * cantidadDeSuenios unaPersona
+
+coeficienteDeSatisfaccion :: Persona -> Int
+coeficienteDeSatisfaccion unaPersona 
+    | enFuncionDeLaFelicidad  unaPersona (edad unaPersona) (cantidadDeSuenios unaPersona) 2 /= 2 = enFuncionDeLaFelicidad  unaPersona (edad unaPersona) (cantidadDeSuenios unaPersona) 2 * felicidonios unaPersona
+    | otherwise = div (felicidonios unaPersona) 2
+
+enFuncionDeLaFelicidad :: Persona -> Int -> Int -> Int -> Int 
+enFuncionDeLaFelicidad unaPersona parametro1 parametro2 parametro3 
+    | tipoDeFelicidad unaPersona == "Muy Feliz" =   parametro1
+    | tipoDeFelicidad unaPersona == "Moderadamente Feliz" =  parametro2
+    | tipoDeFelicidad unaPersona == "Poco Feliz" =  parametro3
+
+{-enFuncionDeLaFelicidad :: Persona -> Int -> Int -> Int -> Int -> Int
+enFuncionDeLaFelicidad unaPersona a b c d 
+    | tipoDeFelicidad unaPersona == "Muy Feliz" =   a*b
+    | tipoDeFelicidad unaPersona == "Moderadamente Feliz" = a*c
+    | tipoDeFelicidad unaPersona == "Poco Feliz" =  a * d 
 coeficienteDeSatisfaccion :: Persona -> Int
 coeficienteDeSatisfaccion unaPersona 
     | tipoDeFelicidad unaPersona == "Muy Feliz" =  edad unaPersona * felicidonios unaPersona
     | tipoDeFelicidad unaPersona == "Moderadamente Feliz" = cantidadDeSuenios unaPersona * felicidonios unaPersona
     | tipoDeFelicidad unaPersona == "Poco Feliz" = div (felicidonios unaPersona) 2
-
 gradoDeAmbicion :: Persona -> Int
 gradoDeAmbicion unaPersona
     | tipoDeFelicidad unaPersona == "Muy Feliz" = felicidonios unaPersona * cantidadDeSuenios unaPersona
     | tipoDeFelicidad unaPersona == "Moderadamente Feliz" = edad unaPersona * cantidadDeSuenios unaPersona 
-    | tipoDeFelicidad unaPersona == "Poco Feliz" = 2 * cantidadDeSuenios unaPersona 
+    | tipoDeFelicidad unaPersona == "Poco Feliz" = 2 * cantidadDeSuenios unaPersona -}
